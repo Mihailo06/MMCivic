684f2d6ff8e870d24aa3049b9c66ae0e025164e6
mihailo06 <mihailojovic006@gmail.com>
2025-11-27 09:26:42 +0100

    feat: Milestone1 (#2)
    BREAKING CHANGE: added the entire civic grammar including extensions to the parser
    
    Co-authored-by: mihailo06 <mihai@dell06.>
    Co-authored-by: LordMZTE <lord@mzte.de>
    Reviewed-on: https://git.mzte.de/LordMZTE/mmcivicc/pulls/2
    Reviewed-by: LordMZTE <lord@mzte.de>
    Co-authored-by: mihailo06 <mihailojovic006@gmail.com>
    Co-committed-by: mihailo06 <mihailojovic006@gmail.com>

---
+++ src/scanparse/lexer_util.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+int lexerDigitExponent(char *text, int length) {
+    int number;
+    char digits[length + 1];
+    char exponent[length + 1];
+    for (int i = 0; i < length; i++) {
+        if (text[i] >= '0' && text[i] <= '9') {
+            digits[i] = text[i];
+        } else if (text[i] == 'e' || text[i] == 'E') {
+            int index = 0;
+            for (int j = i + 1; j < length; j++) {
+                if (text[j] >= '0' && text[j] <= '9') {
+                    exponent[index++] = text[j];
+                }
+            }
+            break;
+        }
+    }
+    int expo = atoi(exponent);
+    number = atoi(digits);
+    for (int i = 0; i < expo; i++) {
+        number = number * 10;
+    }
+    return number;
+}
+
+float lexerFloatExponent(char *text, int length) {
+    float number;
+    char digits[length + 1];
+    char exponent[length + 1];
+    for (int i = 0; i < length; i++) {
+        if ((text[i] >= '0' && text[i] <= '0') || text[i] == '.') {
+            digits[i] = text[i];
+        } else if (text[i] == 'e' || text[i] == 'E') {
+            int index = 0;
+            for (int j = i + 1; j < length; j++) {
+                if (text[j] >= '0' && text[j] <= '0') {
+                    exponent[index++] = text[j];
+                }
+            }
+            break;
+        }
+    }
+    int expo = atoi(exponent);
+    number = strtof(digits, NULL);
+    for (int i = 0; i < expo; i++) {
+        number = number * 10;
+    }
+    return number;
+}
