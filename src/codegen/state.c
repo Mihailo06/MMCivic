f7b33d8469860f97717d042f58d595b45d21b365
LordMZTE <lord@mzte.de>
2026-02-14 12:53:17 +0100

    fix(codegen): workable logic for return instructions

--- src/codegen/state.c
+++ src/codegen/state.c
@@ -4,17 +4,6 @@
 
 #include "util.h"
 
-static const char *returnInstruction(enum BasicType type) {
-    switch (type) {
-        case BT_NULL:  return "return";
-        case BT_bool:  return "breturn";
-        case BT_int:   return "ireturn";
-        case BT_float: return "freturn";
-    }
-
-    return NULL; // unreachable
-}
-
 void codegen_emit(codegen_state *state, FILE *to) {
     fwrite_all(to, state->header.ptr, state->header.len);
     fputc('\n', to);
@@ -23,6 +12,5 @@ void codegen_emit(codegen_state *state, FILE *to) {
         fprintf(to, "%s:\n", func->label_name);
         fprintf(to, CODEGEN_INDENT "esr %zu\n", func->n_locals);
         fwrite_all(to, func->content.ptr, func->content.len);
-        fprintf(to, CODEGEN_INDENT "%s\n\n", returnInstruction(func->return_type));
     }
 }
