5b615b2d330b1aa205be7b7029696ac0ddfc1fc6
LordMZTE <lord@mzte.de>
2026-02-23 13:50:16 +0100

    fix(codegen): respect --output flag

--- src/global/globals.h
+++ src/global/globals.h
@@ -2,13 +2,14 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
 
 struct globals {
     int   line;
     int   col;
     int   verbose;
     char *input_file;
-    char *output_file;
+    FILE *output_file;
 };
 
 extern struct globals global;
